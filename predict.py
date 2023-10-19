ea8661908f8e64c31f1ade65cec86377466c6140
Robin Dong <robin.k.dong@gmail.com>
2023-10-19 19:23:39 +1100

    Add cropout to save sub-images automatically

--- predict.py
+++ predict.py
@@ -7,19 +7,20 @@ from ultralytics.utils.plotting import Annotator
 model = YOLO("best.pt")
 
 # Use the model
-for file in ["pb1.webp", "pb2.webp", "pb3.jpeg", "pb4.webp", "pb5.jpeg"]:
-	img = cv2.imread(f"/Users/robin/Downloads/{file}")  # predict on an image
+for file in ["plage.jpg", "pb1.webp", "pb2.webp", "pb3.jpeg", "pb4.webp", "pb5.jpeg", "bp1.webp", "bp2.avif"]:
+    img = cv2.imread(f"/Users/robin/Downloads/{file}")  # predict on an image
 
-	results = model.predict(img)
-	for res in results:
-		annotator = Annotator(img)
-		boxes = res.boxes
-		for box in boxes:
-			b = box.xyxy[0]
-			c = box.cls
-			annotator.box_label(b, model.names[int(c)])
+    results = model.predict(img)
+    for res in results:
+        annotator = Annotator(img)
+        boxes = res.boxes
+        for box in boxes:
+            b = box.xyxy[0]
+            print(b)
+            c = box.cls
+            annotator.box_label(b, model.names[int(c)])
 
-	img = annotator.result()
-	cv2.imshow('Detection', img)
-	cv2.waitKey(0)
+    img = annotator.result()
+    cv2.imshow('Detection', img)
+    cv2.waitKey(0)
 # path = model.export(format="onnx")  # export the model to ONNX format
