f3e8562fc7e785b247fd6565dde3ec40aeed4678
Robin Dong <robin.k.dong@gmail.com>
2024-09-11 21:22:14 -0400

    Where is the shrimp images going?

--- dataset/build.sh
+++ dataset/build.sh
@@ -6,22 +6,23 @@ rm -rf coco
 cp -r ../../yolo_dataset/coco_backup coco
 
 # prepare basic
-rm -rf yolotrain
-tar xf ../../yolo_dataset/yolotrain.tar
 
-# copy extra into
-for pair in "M/ mammal.dataset.v1.1/" "A/ amphibia.v1.1/" "R/ reptile.v1/"; do
-  dir=( $pair )
-  for file in `find ../../BARM/V1.2.extra.20240523/${dir[0]} -type f`; do
-    cp ${file} yolotrain/${dir[1]}
-  done
-done
+BARM=/media/data2/tetrapod/yolotraining
 
-cp -r ../../BARM/V1.2.extra.20240523/B yolotrain/bird
-cp -r ../../BARM/V1.2.extra.20240523/F yolotrain/fish
-cp -r ../../BARM/extra.20240606-18fishes-461pangolins yolotrain/extra
+mkdir -p yolotrain
+cp -r ${BARM}/V1.2.extra.20240523/A yolotrain/amphibia
+cp -r ${BARM}/V1.2.extra.20240523/B yolotrain/bird
+cp -r ${BARM}/fish.v1.20240525 yolotrain/fish
+cp -r ${BARM}/V1.2.extra.20240523/M yolotrain/mammal
+cp -r ${BARM}/V1.2.extra.20240523/R yolotrain/reptile
+cp ${BARM}/amphibia.v1.1/* yolotrain/amphibia/
+cp ${BARM}/birds.video.20240727/* yolotrain/bird/
+cp ${BARM}/mammal.dataset.v1.1/* yolotrain/mammal/
+cp ${BARM}/reptile.v1/* yolotrain/reptile/
+cp -r ${BARM}/extra.20240606-18fishes-461pangolins yolotrain/extra
+cp -r ${BARM}/shrimpandcrab.V1.20240724 yolotrain/shrimp
 
-DIRS="yolotrain/mammal.dataset.v1.1/ yolotrain/amphibia.v1.1/ yolotrain/reptile.v1/ yolotrain/bird yolotrain/fish yolotrain/extra"
+DIRS="yolotrain/amphibia yolotrain/bird yolotrain/mammal yolotrain/reptile yolotrain/fish yolotrain/extra yolotrain/shrimp"
 # valid
 for file in `find ${DIRS} -name "*5.jpg" -type f`; do
   mv ${file} coco/images/val2017/
@@ -33,7 +34,9 @@ for file in `find ${DIRS} -name "*5.txt" -type f`; do
   # reptile(2) -> 202
   # amphibia(3) -> 203
   # fish(4) -> 204
-  sed s/^0\ /200\ / ${file} | sed s/^1\ /201\ / | sed s/^2\ /202\ / | sed s/^3\ /203\ / | sed s/^4\ /204\ / > coco/labels/val2017/${name}
+  # shrimp(5) -> 205
+  # others(6) -> 206
+  sed s/^0\ /200\ / ${file} | sed s/^1\ /201\ / | sed s/^2\ /202\ / | sed s/^3\ /203\ / | sed s/^4\ /204\ / | sed s/^5\ /205\ / | sed s/^6\ /206\ / > coco/labels/val2017/${name}
   rm ${file}
 done
 
@@ -48,7 +51,9 @@ for file in `find ${DIRS} -name "*.txt" -type f`; do
   # reptile(2) -> 202
   # amphibia(3) -> 203
   # fish(4) -> 204
-  sed s/^0\ /200\ / ${file} | sed s/^1\ /201\ / | sed s/^2\ /202\ / | sed s/^3\ /203\ / | sed s/^4\ /204\ / > coco/labels/train2017/${name}
+  # shrimp(5) -> 205
+  # others(6) -> 206
+  sed s/^0\ /200\ / ${file} | sed s/^1\ /201\ / | sed s/^2\ /202\ / | sed s/^3\ /203\ / | sed s/^4\ /204\ / | sed s/^5\ /205\ / | sed s/^6\ /206\ / > coco/labels/train2017/${name}
   rm ${file}
 done
 
@@ -60,8 +65,4 @@ cd -
 
 python3 clear_categories.py
 
-#cd coco
-#find ./images -type f|grep train > train2017.txt
-#find ./images -type f|grep val > val2017.txt
-#rm -rf train2017.cache val2017.cache
-#cd -
+echo "Finished!"
