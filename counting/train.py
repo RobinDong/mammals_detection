5eaeae1587cb0d2c5c46c72a48b57731e52916ce
Robin Dong <robin.k.dong@gmail.com>
2024-12-03 20:13:37 +1100

    Add dataset of bird-counting and training script

---
+++ counting/train.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO('yolov8l.pt')
+# model = YOLO("/media/data2/sanbai/mammals_detection/runs/detect/train11/weights/last.pt")
+
+# Train the model
+# results = model.train(data='coco.yaml', epochs=600, batch=20, imgsz=640, save_period=2, resume=True, cache=False, device=0)
+results = model.train(data='fbd.yaml', epochs=600, batch=24, imgsz=800, save_period=2, resume=False, device=0, cache=False)
+
+model.export(format="onnx", int8=True)
+model.export(format="openvino", imgsz=640, **{"int8": True, "data": "coco.yaml"})
